ef456a45e5cd97ef777a7272210d17dfd1b3a2c8
georgekhan99 <georgekhan99@gmail.com>
2022-07-29 16:03:50 +0700

    MEVN Tutorials

---
+++ src/components/EditComponent.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <!-- Contents -->
+
+
+    </div>
+</div>
+  
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+
+        }
+     }
+}
+</script>
+
+<style>
+
+</style>
